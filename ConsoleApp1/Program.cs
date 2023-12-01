41512279466fe663fd6ff8e9b89ad8318cfa74ea
Master Yoda <masteryodamaster@outlook.com>
2023-12-01 17:50:11 +0200

    Email validation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+
+var emailCompanies = new EmailCompanies();
+emailCompanies.TryAdd("e@g.com", 1);
+
+Console.WriteLine("Hello, World!");
+
+
+internal class EmailCompanies
+{
+    private readonly IDictionary<string, HashSet<int>> _dictionaryCompanies = new Dictionary<string, HashSet<int>>();
+    private readonly EmailAddressAttribute _emailValidator = new();
+
+    public void TryAdd(string email, IEnumerable<int> companies)
+    {
+        if (EmailIsValid(email))
+        {
+            if (_dictionaryCompanies.TryGetValue(email, out var currentCompanies))
+            {
+                currentCompanies.UnionWith(companies);
+            }
+            else
+            {
+                _dictionaryCompanies.Add(email, new HashSet<int>(companies));
+            }
+        }
+    }
+
+    public void TryAdd(string email, int company)
+    {
+        if (EmailIsValid(email))
+        {
+            if (_dictionaryCompanies.TryGetValue(email, out var currentCompanies))
+            {
+                currentCompanies.Add(company);
+            }
+            else
+            {
+                _dictionaryCompanies.Add(email, new HashSet<int>() { company });
+            }
+        }
+    }
+
+    public IEnumerable<int>? GetCompaniesByEmail(string email)
+    {
+        if (EmailIsValid(email))
+        {
+            if (_dictionaryCompanies.TryGetValue(email, out var companies))
+            {
+                return companies;
+            }
+        }
+        return null;
+    }
+
+    private bool EmailIsValid(string email)
+    {
+        return !string.IsNullOrEmpty(email) && _emailValidator.IsValid(email);
+    }
+}
